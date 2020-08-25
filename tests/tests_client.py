0e3411ebda6cd2b6f6c18c56036bfedd14720cbd
wilmeryan <wilmeryan@sypht.com>
2020-08-25 07:25:44 +0000

    added test cases for reauthentication

---
+++ tests/tests_client.py
@@ -0,0 +1,89 @@
+import os
+import unittest
+import json
+import six
+
+import warnings
+
+from sypht.client import SyphtClient
+
+from uuid import UUID
+from datetime import datetime, timedelta
+
+def validate_uuid4(uuid_string):
+    try:
+        val = UUID(uuid_string, version=4)
+    except ValueError:
+        return False
+
+    return val.hex == uuid_string.replace('-', '')
+
+
+class DataExtraction(unittest.TestCase):
+    def setUp(self):
+        if not six.PY2:
+            warnings.simplefilter('ignore', category=ResourceWarning)
+
+        self.sypht_client = SyphtClient(os.environ['CLIENT_ID'], os.environ['CLIENT_SECRET'])
+
+    def test_with_wrong_fieldset(self):
+        with self.assertRaises(Exception) as context:
+            with open('tests/sample_invoice.pdf', 'rb') as f:
+                response = self.sypht_client.upload(f, fieldsets=['sypht.incorrect', ])
+                self.assertIn('does not have permission to use fieldSet sypht.incorrect', reponse['error'])
+
+        self.assertTrue('Request failed with status code' in str(context.exception))
+
+    def test_data_extraction_1(self):
+        with open('tests/sample_invoice.pdf', 'rb') as f:
+            fid = self.sypht_client.upload(f, fieldsets=["sypht.invoice"])
+            self.assertTrue(validate_uuid4(fid))
+
+        results = self.sypht_client.fetch_results(fid)
+
+        self.assertTrue(isinstance(results, dict))
+        self.assertIn('invoice.dueDate', results)
+        self.assertIn('invoice.total', results)
+        self.assertIn('invoice.amountPaid', results)
+        self.assertIn('invoice.amountDue', results)
+
+    def test_data_extraction_2(self):
+        with open('tests/sample_invoice.pdf', 'rb') as f:
+            fid = self.sypht_client.upload(f, fieldsets=[ "sypht.invoice", "sypht.bank" ])
+            self.assertTrue(validate_uuid4(fid))
+
+        results = self.sypht_client.fetch_results(fid)
+
+        self.assertTrue(isinstance(results, dict))
+        self.assertIn('invoice.dueDate', results)
+        self.assertIn('invoice.total', results)
+        self.assertIn('invoice.amountPaid', results)
+        self.assertIn('invoice.amountDue', results)
+        self.assertIn('bank.accountNo', results)
+        self.assertIn('bank.bsb', results)
+
+
+class ReauthenticateTest(unittest.TestCase):
+    def setUp(self):
+        self.sypht_client = SyphtClient()
+        self.init_access_token = self.sypht_client._access_token
+        self.assertFalse(self.sypht_client._is_token_expired())
+    
+    def test_no_reauthentication(self):
+        # Test non-expired token doesn't require reauthentication
+        self.sypht_client.get_company()
+        self.assertEqual(self.init_access_token, self.sypht_client._access_token)
+    
+    def test_reauthentication(self):
+        # Set auth expiry to 1 second ago to avoid mocking datetime
+        self.sypht_client._auth_expiry = datetime.utcnow() - timedelta(seconds=1)        
+        self.assertTrue(self.sypht_client._is_token_expired())
+        
+        # Get request will auto-reauthenticate
+        self.sypht_client.get_company()
+        self.assertNotEqual(self.init_access_token, self.sypht_client._access_token)
+        self.assertFalse(self.sypht_client._is_token_expired())
+        
+        
+if __name__ == '__main__':
+    unittest.main()
