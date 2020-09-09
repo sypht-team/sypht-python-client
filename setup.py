56639e49a4672ebb54e14bb07786cf00a56fdeeb
Andy Chisholm <andy@sypht.com>
2020-09-09 11:02:39 +0000

    Bump version to 0.4.4

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.4.3'
-__pkg_name__ = 'sypht'
+__version__ = "0.4.4"
+__pkg_name__ = "sypht"
 
 setup(
-    name = __pkg_name__,
-    version = __version__,
-    description = 'Sypht Python Client',
-    author='Sypht Pty Ltd.',
-    packages = find_packages(),
-    url = 'https://sypht.com',
-    entry_points={
-        'console_scripts': [
-            'sypht = sypht.__main__:main'
-        ]
-    },
+    name=__pkg_name__,
+    version=__version__,
+    description="Sypht Python Client",
+    author="Sypht Pty Ltd.",
+    packages=find_packages(),
+    url="https://sypht.com",
+    entry_points={"console_scripts": ["sypht = sypht.__main__:main"]},
     classifiers=[
-        'Environment :: Console',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+        "Environment :: Console",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    install_requires=[
-        'requests>=2.20.1,<3',
-        'six>=1.11.0,<2'
-    ]
+    install_requires=["requests>=2.20.1,<3", "six>=1.11.0,<2"],
 )
